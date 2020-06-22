4594741f5d9e983d1d74479ddbb96f10afa58712
jiangdianbao <16600609@qq.com>
2020-06-22 10:13:46 +0800

    first commit

---
+++ src/com/jdb/test/MainTest1.java
@@ -0,0 +1,11 @@
+package com.jdb.test;
+
+/**
+ * @author jiangdianbao
+ * @date 2020/6/22 10:09
+ */
+public class MainTest1 {
+    public static void main(String[] args) {
+        System.out.println("hshh ");
+    }
+}
