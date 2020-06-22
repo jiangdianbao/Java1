65a9d9fe5f370b1100600acfb33f3ed88a74bc35
jiangdianbao <16600609@qq.com>
2020-06-22 10:51:04 +0800

    commmit4

--- src/com/jdb/test/MainTest4.java
+++ src/com/jdb/test/MainTest4.java
@@ -1,12 +1,11 @@
 package com.jdb.test;
 
-
 /**
  * @author jiangdianbao
- * @date 2020/6/22 10:28
+ * @date 2020/6/22 10:49
  */
-public class MainTest3 {
+public class MainTest4 {
     public static void main(String[] args) {
-        System.out.println("342342131423 dsfs");
+        System.out.println("dfsafdsfsd");
     }
 }
