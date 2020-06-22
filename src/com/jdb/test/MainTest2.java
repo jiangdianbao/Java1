678b9ec12c24704328dca84493196b2467255220
jiangdianbao <16600609@qq.com>
2020-06-22 10:24:47 +0800

    commit 2dev1

---
+++ src/com/jdb/test/MainTest2.java
@@ -0,0 +1,11 @@
+package com.jdb.test;
+
+/**
+ * @author jiangdianbao
+ * @date 2020/6/22 10:23
+ */
+public class MainTest2 {
+    public static void main(String[] args) {
+        System.out.println("sdfdsj");
+    }
+}
